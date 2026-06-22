cf453fe4ff8168264003dd0c5534dd31c133e537
kiruthika <m3892366@gmail.com>
2026-06-22 13:08:10 +0530

    first

---
+++ DBConnection.java
@@ -0,0 +1,28 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+
+    public static Connection connect() {
+        try {
+            
+            Connection con =
+                DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/hms",
+                    "root",
+                    "Kiru@123"
+                );
+                
+                
+                
+            System.out.println("Database Connected");
+           
+    
+            return con;
+
+        } catch (Exception e) {
+            System.out.println(e);
+            return null;
+        }
+    }
+}
